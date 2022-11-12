d8380c16eafa51089e2680a4750ec0d93276b545
Ahmed Khalaf Hassan <Ahmed Khalaf Hassan@DESKTOP-BRBO08O>
2022-11-12 16:12:49 +0200

    web form

---
+++ Home.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                Label1.Text = "Welcome " + ((User)Session["user"]).Username;
+            }
+        }
+    }
+}
---
+++ Login.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            testEntities test = new testEntities();
+            User user = test.Users.FirstOrDefault(u => u.Username == TextBox1.Text && u.Password == TextBox2.Text);
+            if (user == null)
+            {
+                Label1.Visible = true;
+            }
+            else
+            {
+                Session["user"] = user;
+                Response.Redirect("Home.aspx");
+            }
+        }
+    }
+}
